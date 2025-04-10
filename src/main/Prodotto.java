69588679ba0bfc940d91529c3a3a4f5bcc90303a
m0nica1998 <monica.cesario98@icloud.com>
2025-04-10 19:20:03 +0200

    init classe Prodotto

---
+++ src/main/Prodotto.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Prodotto {
+	//## java-inheritance-shop Nel progetto java-inheritance-shop
+		//Creare la classe Prodotto che gestisce i prodotti dello shop. 
+		//Un prodotto è caratterizzato da: codice (numero intero) nome marca prezzo iva
+		//Usate opportunamente i livelli di accesso (public, private), i costruttori, 
+		//i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che: 
+		//il codice prodotto sia accessibile solo in lettura gli altri attributi siano accessibili sia 
+		//in lettura che in scrittura il prodotto esponga sia un metodo per avere il prezzo base che uno
+		//per avere il prezzo comprensivo di iva formattato
+	
+	//creare gli attributi (codice, nome, marca,prezzo e iva)
+	//creare i costruttori
+	//metodi getter setter
+	//mettere il setter privato per codice
+	//metodo esporre prezzo
+	//metodo prezzo con iva
+	
+	
+}
