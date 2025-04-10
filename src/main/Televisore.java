9c225482e551b2108d68f5da4b1c29eae57d61bf
m0nica1998 <monica.cesario98@icloud.com>
2025-04-10 19:42:09 +0200

    init class televisore

---
+++ src/main/Televisore.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Televisore extends Prodotto{
+
+	 //Televisori, 
+	//caratterizzati dalle dimensioni e dalla proprietà di essere smart oppure no
+	
+	public Televisore(String nome, String marca, double prezzo, double iva) {
+		super(nome, marca, prezzo, iva);
+		// TODO Auto-generated constructor stub
+	}
+
+}
