054844d0c7ef6d8ce89263b14f6494ff81df80c7
m0nica1998 <monica.cesario98@icloud.com>
2025-04-10 19:51:42 +0200

    add and implement class cuffia

--- src/main/Cuffia.java
+++ src/main/Cuffia.java
@@ -1,5 +1,38 @@
 package main;
 
-public class Cuffia {
+public class Cuffia extends Prodotto{
+	//Cuffie,
+	//caratterizzate dal colore e se sono wireless o cablate
+	
+	//attributi
+	private String colore;
+	private boolean isWireless;
+
+	public Cuffia(String nome, String marca, double prezzo, double iva, String colore, boolean isWireless) {
+		super(nome, marca, prezzo, iva);
+		// TODO Auto-generated constructor stub
+		setColore(colore);
+		setWireless(isWireless);
+	}
+
+	//getter e setter
+	public String getColore() {
+		return colore;
+	}
+
+	public void setColore(String colore) {
+		if(colore != "") {
+			this.colore = colore;
+		}
+		
+	}
+
+	public boolean isWireless() {
+		return isWireless;
+	}
+
+	public void setWireless(boolean isWireless) {
+		this.isWireless = isWireless;
+	}
 
 }
