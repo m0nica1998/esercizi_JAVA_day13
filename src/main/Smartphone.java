36001864a989d34fc0b91baee8b9a6d86df8b9df
m0nica1998 <monica.cesario98@icloud.com>
2025-04-10 19:40:03 +0200

    add class smartphone

---
+++ src/main/Smartphone.java
@@ -0,0 +1,35 @@
+package main;
+
+public class Smartphone extends Prodotto{
+
+	//Lo shop gestisce diversi tipi di prodotto: 
+	//Smarphone, caratterizzati anche dal codice IMEI e dalla quantità di memoria
+	
+	//attributi
+	private String imei;
+	private int memoria;
+	public Smartphone(String nome, String marca, double prezzo, double iva, String imei, int memoria) {
+		super(nome, marca, prezzo, iva);
+		setImei(imei);
+		setMemoria(memoria);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	//getter e setter
+	public String getImei() {
+		return imei;
+	}
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+
+	public int getMemoria() {
+		return memoria;
+	}
+
+	public void setMemoria(int memoria) {
+		this.memoria = memoria;
+	}
+	
+}
